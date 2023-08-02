b38559a6c5c79ba6a03ed6e9d99f4b936b8fb8d9
sungjun son <o_nj_u@naver.com>
2023-08-02 15:07:26 +0900

    react-icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "../styles/Contact.css";
-import Github from "../images/github-logo.svg";
+import { FaGithub, FaArrowUp } from "react-icons/fa";
 
 const Contact = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <>
       <div className="contact" id="contact">
         <h1 className="contact-title">Contact</h1>
         <p className="email">sjson666@gmail.com</p>
         <a href="https://github.com/sjson555?tab=repositories">
-          <img src={Github} alt="github" className="github"></img>
+          <FaGithub className="github" />
         </a>
+      </div>
+      <div className="scroll-to-top" onClick={handleScrollToTop}>
+        <FaArrowUp className="arrow-up" />
       </div>
     </>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,25 @@
 import React from "react";
-import html5 from "../images/html5.svg";
-import css3 from "../images/css3.svg";
-import javascript from "../images/javascript.svg";
-import react from "../images/react.svg";
-import java from "../images/java.svg";
-import spring from "../images/spring.svg";
-import springboot from "../images/springboot.svg";
-import mysql from "../images/mysql.svg";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaJava } from "react-icons/fa";
+import { SiSpring, SiSpringboot, SiMysql } from "react-icons/si";
 import "../styles/Skills.css";
 
 const Skills = () => {
   return (
     <div className="skills">
       <div className="skills-title">
-        <h1 className="hedding">Skills</h1>
+        <h1 className="heading">Skills</h1>
       </div>
       <div className="front">
-        <img src={html5} alt="html5" className="skills-img"></img>
-        <img src={css3} alt="css3" className="skills-img"></img>
-        <img src={javascript} alt="javascript" className="skills-img"></img>
-        <img src={react} alt="react" className="skills-img"></img>
+        <FaHtml5 className="skills-icon" />
+        <FaCss3Alt className="skills-icon" />
+        <FaJs className="skills-icon" />
+        <FaReact className="skills-icon" />
       </div>
       <div className="back">
-        <img src={java} alt="java" className="skills-img"></img>
-        <img src={spring} alt="spring" className="skills-img"></img>
-        <img src={springboot} alt="springboot" className="skills-img"></img>
-        <img src={mysql} alt="mysql" className="skills-img"></img>
+        <FaJava className="skills-icon" />
+        <SiSpring className="skills-icon" />
+        <SiSpringboot className="skills-icon" />
+        <SiMysql className="skills-icon" />
       </div>
     </div>
   );
