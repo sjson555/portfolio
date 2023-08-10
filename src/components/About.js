b904e348dcd2dbc91ce221832bdffbdc48c4fdf7
sungjun son <o_nj_u@naver.com>
2023-08-10 15:26:35 +0900

    About2

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,16 @@ const About = () => {
     <div className="about-me" id="about">
       <div className="about-me-title">
         <h1 className="hedding">About Me</h1>
+        <div className="about-me-photo">
+          <img src="" alt="profile" />
+        </div>
       </div>
       <div className="about-me-content">
-        <h1>title</h1>
+        <p>
+          안녕하세요. 풀스택 개발자를 꿈꾸는 손성준입니다.
+          <br />
+          저는 새로운 기술을 배우면서 무언가를 만드는 것을 좋아합니다.
+        </p>
         <section className="about-me-section">
           <p>
             2023.01.09 ~ 2023.06.23
