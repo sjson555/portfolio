3edec81fea0cead67621e943279468b36e477c58
sungjun son <o_nj_u@naver.com>
2023-08-28 19:22:29 +0900

    am

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,8 +9,9 @@ const About = () => {
       </div>
       <div className="about-me-content">
         <p>
-          안녕하세요.
+          안녕하세요!
           <br />
+          저는 프론트엔드와 백엔드를 학습하며 더욱 성장할 기회를 찾고있습니다.
         </p>
         <section className="about-me-section">
           <p>
