b73c5e87b799497743d2bdc804d4659962a6fef7
sungjun son <o_nj_u@naver.com>
2024-01-08 17:11:35 +0900

    20240108 스킬표시방법수정

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,6 +3,11 @@ import { motion } from "framer-motion";
 import mysqlSVG from "../svg/mysql.svg";
 import reactSVG from "../svg/react.svg";
 import springbootSVG from "../svg/springboot.svg";
+import htmlSVG from "../svg/html5.svg";
+import cssSVG from "../svg/css3.svg";
+import javascriptSVG from "../svg/javascript.svg";
+import springSVG from "../svg/spring.svg";
+import javaSVG from "../svg/java.svg";
 import { useMouseVariant } from "../modules/customMouse/hooks/useMouseVariant";
 import "../styles/Skills.css";
 
@@ -32,10 +37,25 @@ const Skills = () => {
       <div className="skills-title">
         <div className="skills-hedding">Skill</div>
       </div>
-      <div className="skills-icon-list">
-        <SkillIcon svg={reactSVG} name="React" />
-        <SkillIcon svg={springbootSVG} name="SpringBoot" />
-        <SkillIcon svg={mysqlSVG} name="MySQL" />
+      <div className="skills-section">
+        <div className="skills-category">
+          <div className="skills-hedding">Frontend</div>
+          <div className="skills-icon-list">
+            <SkillIcon svg={htmlSVG} name="HTML" />
+            <SkillIcon svg={cssSVG} name="CSS" />
+            <SkillIcon svg={javascriptSVG} name="JavaScript" />
+            <SkillIcon svg={reactSVG} name="React" />
+          </div>
+        </div>
+        <div className="skills-category">
+          <div className="skills-hedding">Backend</div>
+          <div className="skills-icon-list">
+            <SkillIcon svg={javaSVG} name="JAVA" />
+            <SkillIcon svg={springSVG} name="Spring" />
+            <SkillIcon svg={springbootSVG} name="SpringBoot" />
+            <SkillIcon svg={mysqlSVG} name="MySQL" />
+          </div>
+        </div>
       </div>
     </div>
   );
